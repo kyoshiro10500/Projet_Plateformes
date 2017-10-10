5aea277f5e71aa4fd2087d3fff64a9478c5ab393
Kyoshiro10500 <victorperson10@hotmail.fr>
2017-10-10 11:14:32 +0200

    Plateformer V1.0 : -Ajout de la gravité -Gestion des collisions avec raycast2D -Ajout du dash (modification de la vitesse quand ajout sur une touche -Ajout du saut
    Bug :
    -Saut infinis
    
    TODO :
    -Double saut
    -Wall jump
    - ????

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal_axis = 0f;
+        horizontal_axis = Input.GetAxis("Horizontal");
+        if(horizontal_axis != 0)
+        {
+            this.GetComponent<GravityManager>().Horizontal_Move(horizontal_axis);
+        }	
+
+        if(Input.GetButtonDown("Dash"))
+        {
+            this.GetComponent<GravityManager>().Set_Dash(true);
+        }
+
+        if(Input.GetButtonUp("Dash"))
+        {
+            this.GetComponent<GravityManager>().Set_Dash(false);
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            this.GetComponent<GravityManager>().Set_Jump(true);
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            this.GetComponent<GravityManager>().Set_Jump(false);
+        }
+    }
+}
