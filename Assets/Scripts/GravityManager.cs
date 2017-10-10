f3b30744b795e5432522f9ca4ca67874d418bb7c
Kyoshiro10500 <victorperson10@hotmail.fr>
2017-10-10 15:27:36 +0200

    Plateformer V1.0 : -Ajout des plateformes mobiles
    Bug :
    -Le personnage rentre dans la plateforme quand on collide le coin d'une plateforme avec son coin
    -Le personnage ne suit pas les mouvements des plateformes
    
    TODO :
    -Wall jump

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityManager : MonoBehaviour {
+public class GravityManager : MonoBehaviour
+{
 
     [Header("Engine")]
     [SerializeField]
@@ -32,18 +33,19 @@ public class GravityManager : MonoBehaviour {
 
     private RaycastHit2D ray_collide_down_l;
     private RaycastHit2D ray_collide_down_r;
-    private RaycastHit2D ray_collide_up_l ;
+    private RaycastHit2D ray_collide_up_l;
     private RaycastHit2D ray_collide_up_r;
-    private RaycastHit2D ray_collide_left_u ;
+    private RaycastHit2D ray_collide_left_u;
     private RaycastHit2D ray_collide_left_d;
-    private RaycastHit2D ray_collide_right_u ;
+    private RaycastHit2D ray_collide_right_u;
     private RaycastHit2D ray_collide_right_d;
 
     private float sprite_width;
     private float sprite_height;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         sprite_width = this.GetComponent<Renderer>().bounds.size[0];
         sprite_height = this.GetComponent<Renderer>().bounds.size[1];
         speed_to_use = speed_horizontal;
@@ -51,7 +53,7 @@ public class GravityManager : MonoBehaviour {
 
     public void Set_Jump(bool toActivate)
     {
-        if(toActivate && number_jump < 2f)
+        if (toActivate && number_jump < 2f)
         {
             jump_on = true;
             jump_origin_y = this.transform.position.y;
@@ -66,7 +68,7 @@ public class GravityManager : MonoBehaviour {
 
     public void Set_Dash(bool toActivate)
     {
-        if(toActivate)
+        if (toActivate)
         {
             speed_to_use = speed_horizontal_dash;
         }
@@ -75,11 +77,11 @@ public class GravityManager : MonoBehaviour {
             speed_to_use = speed_horizontal;
         }
     }
-	
+
     public void Horizontal_Move(float horizontal_axis)
     {
 
-        if((horizontal_axis > 0 && ray_collide_right_u.collider != null && Mathf.Abs(ray_collide_right_u.point.x - transform.position.x) >= (sprite_width / 2) + collisionMaxDistanceDetection)
+        if ((horizontal_axis > 0 && ray_collide_right_u.collider != null && Mathf.Abs(ray_collide_right_u.point.x - transform.position.x) >= (sprite_width / 2) + collisionMaxDistanceDetection)
             && (horizontal_axis > 0 && ray_collide_right_d.collider != null && Mathf.Abs(ray_collide_right_d.point.x - transform.position.x) >= (sprite_width / 2) + collisionMaxDistanceDetection))
         {
             if (ray_collide_right_d.point.x <= ray_collide_right_u.point.x)
@@ -106,7 +108,7 @@ public class GravityManager : MonoBehaviour {
             }
         }
 
-        if((horizontal_axis < 0 && ray_collide_left_u.collider != null && Mathf.Abs(ray_collide_left_u.point.x - transform.position.x) >= (sprite_width / 2) + collisionMaxDistanceDetection)
+        if ((horizontal_axis < 0 && ray_collide_left_u.collider != null && Mathf.Abs(ray_collide_left_u.point.x - transform.position.x) >= (sprite_width / 2) + collisionMaxDistanceDetection)
             && (horizontal_axis < 0 && ray_collide_left_d.collider != null && Mathf.Abs(ray_collide_left_d.point.x - transform.position.x) >= (sprite_width / 2) + collisionMaxDistanceDetection))
         {
             if (ray_collide_left_d.point.x >= ray_collide_left_u.point.x)
@@ -124,7 +126,7 @@ public class GravityManager : MonoBehaviour {
             {
                 if (transform.position.x - sprite_width / 2 + horizontal_axis * speed_to_use < ray_collide_left_u.point.x)
                 {
-                    this.transform.Translate(new Vector3(ray_collide_left_u.point.x - transform.position.x + sprite_width / 2 , 0, 0));
+                    this.transform.Translate(new Vector3(ray_collide_left_u.point.x - transform.position.x + sprite_width / 2, 0, 0));
                 }
                 else
                 {
@@ -134,16 +136,17 @@ public class GravityManager : MonoBehaviour {
         }
 
     }
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 pos = this.transform.position;
-        ray_collide_up_l = Physics2D.Raycast(new Vector3(pos[0] - (sprite_width/2) + 0.05f,pos[1],pos[2]), Vector3.up);
-        ray_collide_up_r = Physics2D.Raycast(new Vector3(pos[0] + (sprite_width / 2) -0.05f, pos[1], pos[2]), Vector3.up);
-        ray_collide_down_l =  Physics2D.Raycast(new Vector3(pos[0] - (sprite_width / 2) +0.05f, pos[1], pos[2]), Vector3.down);
-        ray_collide_down_r = Physics2D.Raycast(new Vector3(pos[0] + (sprite_width / 2) -0.05f, pos[1], pos[2]), Vector3.down);
-        ray_collide_left_u = Physics2D.Raycast(new Vector3(pos[0] , pos[1] + (sprite_height / 2) -0.05f, pos[2]), Vector3.left);
+        ray_collide_up_l = Physics2D.Raycast(new Vector3(pos[0] - (sprite_width / 2) + 0.05f, pos[1], pos[2]), Vector3.up);
+        ray_collide_up_r = Physics2D.Raycast(new Vector3(pos[0] + (sprite_width / 2) - 0.05f, pos[1], pos[2]), Vector3.up);
+        ray_collide_down_l = Physics2D.Raycast(new Vector3(pos[0] - (sprite_width / 2) + 0.05f, pos[1], pos[2]), Vector3.down);
+        ray_collide_down_r = Physics2D.Raycast(new Vector3(pos[0] + (sprite_width / 2) - 0.05f, pos[1], pos[2]), Vector3.down);
+        ray_collide_left_u = Physics2D.Raycast(new Vector3(pos[0], pos[1] + (sprite_height / 2) - 0.05f, pos[2]), Vector3.left);
         ray_collide_left_d = Physics2D.Raycast(new Vector3(pos[0], pos[1] - (sprite_height / 2) + 0.05f, pos[2]), Vector3.left);
-        ray_collide_right_u = Physics2D.Raycast(new Vector3(pos[0], pos[1] + (sprite_height / 2) -0.05f, pos[2]), Vector3.right);
+        ray_collide_right_u = Physics2D.Raycast(new Vector3(pos[0], pos[1] + (sprite_height / 2) - 0.05f, pos[2]), Vector3.right);
         ray_collide_right_d = Physics2D.Raycast(new Vector3(pos[0], pos[1] - (sprite_height / 2) + 0.05f, pos[2]), Vector3.right);
 
         if (!jump_on)
@@ -181,7 +184,7 @@ public class GravityManager : MonoBehaviour {
         }
         else
         {
-            if(this.transform.position.y < jump_origin_y + jump_height)
+            if (this.transform.position.y < jump_origin_y + jump_height)
             {
                 if ((ray_collide_up_l.collider != null && Mathf.Abs(ray_collide_up_l.point.y - transform.position.y) >= (sprite_height / 2) + collisionMaxDistanceDetection)
                 && (ray_collide_up_r.collider != null && Mathf.Abs(ray_collide_up_r.point.y - transform.position.y) >= (sprite_height / 2) + collisionMaxDistanceDetection))
@@ -216,13 +219,13 @@ public class GravityManager : MonoBehaviour {
             }
             else
             {
-                if(jump_height_reached == 0)
+                if (jump_height_reached == 0)
                 {
                     jump_height_reached = 0.01f;
                 }
                 else
                 {
-                    if(jump_height_reached >= time_stay_up)
+                    if (jump_height_reached >= time_stay_up)
                     {
                         jump_on = false;
                     }
