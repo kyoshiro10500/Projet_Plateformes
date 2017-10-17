495c0e9366da8e8fc23e7723207a7fb2da759d1d
Thomas BOUTANT <thomasboutant@orange.fr>
2017-10-17 13:27:17 +0200

    plateforme traversable
    (double saut enlevé...)

---
+++ Assets/Assets/Scripts/VerticalPlateforme.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlateforme : MonoBehaviour {
+
+    [SerializeField]
+    private float max_amplitude;
+
+    [SerializeField]
+    private float vitesse;
+
+    [SerializeField]
+    private bool is_horizontal;
+
+    private float min_hauteur;
+	// Use this for initialization
+	void Start () {
+        if(is_horizontal)
+        {
+            min_hauteur = this.transform.position.x - 0.01f;
+        }
+        else
+        {
+            min_hauteur = this.transform.position.y - 0.01f;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(is_horizontal)
+        {
+            if (this.transform.position.x <= min_hauteur || this.transform.position.x >= min_hauteur + max_amplitude)
+            {
+                vitesse = -vitesse;
+            }
+
+            this.transform.Translate(new Vector3(Time.deltaTime * vitesse, 0, 0));
+        }
+        else
+        {
+            if (this.transform.position.y <= min_hauteur || this.transform.position.y >= min_hauteur + max_amplitude)
+            {
+                vitesse = -vitesse;
+            }
+
+            this.transform.Translate(new Vector3(0, Time.deltaTime * vitesse, 0));
+        }
+	}
+}
